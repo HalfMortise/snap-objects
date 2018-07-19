6977a79b0963e5e5c2df0f0cc7fd22cf31ae0f32
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-19 08:37:48 -0600

    Created class and added two variables

---
+++ objects.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ *
+ */
+
+class Computer {
+
+   /**
+    *id for this computer model
+    * @var String $computerModel
+    */
+   private $computerModel;
+
+   /**
+    *id for this computer mac address
+    * @var Integer $computerMacAddress
+    */
+   private $computerMacAddress;
+
+   /**
+    *
+    *
+    */
+
+}
