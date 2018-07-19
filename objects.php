77380431db69a4fda49c086ce37e83e15c7658d9
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-19 08:45:40 -0600

    Class completed

--- objects.php
+++ objects.php
@@ -19,8 +19,29 @@ class Computer {
    private $computerMacAddress;
 
    /**
-    *
-    *
+    *accessor method for computer model
+    * @return string value of computer model
     */
+   public function getComputerMOdel() : String {
+      return($this->computerModel);
+   }
 
+   /**
+    * mutator method for computer model
+    *
+    * @param String $newComputerModel new value of computer model
+    * @throws \RangeException if $newComputerModel is not unique
+    * @throws \TypeError if $newComputerModel is not a string
+    **/
+   public function setComputerModel( $newComputerModel) : void {
+      try {
+         $string = self::validateString($newComputerModel);
+      } catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+         $exceptionType = get_class($exception);
+         throw(new $exceptionType($exception->getMessage(), 0, $exception));
+      }
+
+      // convert and store the computer model
+      $this->computerModel = $string;
+   }
 }
